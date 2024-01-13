a9fedcf953fdaf5be709620bd7de20ade3f7b12d
Martini310 <martin310@wp.pl>
2024-01-13 13:42:09 +0100

    Exercise 3_4

--- stock.py
+++ stock.py
@@ -2,18 +2,45 @@ import csv
 
 
 class Stock:
-    types = (str, int, float)
+    _types = (str, int, float)
+
+    __slots__ = ('name', '_shares', '_price')
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError(f"Expected {self._types[1].__name__}")
+        if value < 0:
+            raise ValueError("shares must be >= 0")
+        self._shares = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError(f"Expected {self._types[2].__name__}")
+        if value < 0:
+            raise ValueError("price must be >= 0")
+        self._price = value
+
     @classmethod
     def from_row(cls, row):
-        values = [func(val) for func, val in zip(cls.types, row)]
+        values = [func(val) for func, val in zip(cls._types, row)]
         return cls(*values)
 
+    @property
     def cost(self):
         return self.shares * self.price
 
@@ -49,12 +76,11 @@ class DStock(Stock):
 if __name__ == '__main__':
     portfolio = read_portfolio('Data/portfolio.csv', DStock)
     print_portfolio(portfolio)
-    a = Stock('test', 22, 1)
+    a = Stock('test', 22, 1.9)
     row = ['AA', '100', '32.20']
     s = DStock.from_row(row)
     print(s.name)
     print(s.shares)
     print(s.price)
-    print(s.cost())
-
+    print(s.cost)
 
