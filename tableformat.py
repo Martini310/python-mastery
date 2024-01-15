28aa79294dadfcdb9b1990de74a18e8c29e77c4a
Martini310 <martin310@wp.pl>
2024-01-15 16:38:33 +0100

    Exercise 3_5

--- tableformat.py
+++ tableformat.py
@@ -1,14 +1,74 @@
 from stock import read_portfolio, Stock
+import reader
 
 
-def print_table(list_of_obj, attr_names):
-    print(''.join(f'{name:>10}' for name in attr_names))
-    print(f'{("-" * 10 + " ") * len(attr_names)}')
-    for obj in list_of_obj:
-        print(''.join(f"{getattr(obj, name):>10}" for name in attr_names))
+# def print_table(list_of_obj, attr_names):
+#     print(''.join(f'{name:>10}' for name in attr_names))
+#     print(f'{("-" * 10 + " ") * len(attr_names)}')
+#     for obj in list_of_obj:
+#         print(''.join(f"{getattr(obj, name):>10}" for name in attr_names))
+def print_table(records, fields, formatter):
+    formatter.headings(fields)
+    for r in records:
+        rowdata = [getattr(r, fieldname) for fieldname in fields]
+        formatter.row(rowdata)
+
+
+class TableFormatter:
+    def headings(self, headers):
+        raise NotImplementedError()
+
+    def row(self, rowdata):
+        raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(' '.join('%10s' % h for h in headers))
+        print(('-' * 10 + ' ') * len(headers))
+
+    def row(self, rowdata):
+        print(' '.join('%10s' % d for d in rowdata))
+
+
+class CSVTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(str(row) for row in rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print('<tr>', end='')
+        for header in headers:
+            print(f'<th>{header}</th>', end='')
+        print('</tr>')
+
+    def row(self, rowdata):
+        print('<tr>', end='')
+        for r in rowdata:
+            print(f'<td>{r}</td>', end='')
+        print('</tr>')
+
+
+def create_formatter(name):
+    if name == 'text':
+        formatter_cls = TextTableFormatter
+    elif name == 'csv':
+        formatter_cls = CSVTableFormatter
+    elif name == 'html':
+        formatter_cls = HTMLTableFormatter
+    else:
+        raise RuntimeError('Unknown format %s' % name)
+    return formatter_cls()
 
 
 if __name__ == '__main__':
-    portfolio = read_portfolio('Data/portfolio.csv', Stock)
-    print_table(portfolio, ['name', 'shares', 'price'])
-    print_table(portfolio, ['shares', 'name'])
+    # portfolio = read_portfolio('Data/portfolio.csv', Stock)
+    # print_table(portfolio, ['name', 'shares', 'price'])
+    # print_table(portfolio, ['shares', 'name'])
+    portfolio = reader.read_csv_as_instances('Data/portfolio.csv', Stock)
+    formatter = create_formatter('csv')
+    print_table(portfolio, ['name', 'shares', 'price'], formatter)
