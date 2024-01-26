77e77b9c4fd48466cef3722bfdaa9d8628052a8a
Martini310 <martin310@wp.pl>
2024-01-26 13:43:19 +0100

    Exercise 6_4

--- stock.py
+++ stock.py
@@ -4,9 +4,6 @@ from structure import Structure
 class Stock(Structure):
     _fields = ('name', 'shares', 'price')
 
-    def __init__(self, name, shares, price):
-        self._init()
-
     @property
     def cost(self):
         return self.shares * self.price
@@ -15,7 +12,7 @@ class Stock(Structure):
         self.shares -= nshares
 
 
-Stock.set_fields()
+Stock.create_init()
 
 if __name__ == '__main__':
 
@@ -25,4 +22,5 @@ if __name__ == '__main__':
     s = Stock('GOOG', 490.1, 50)
     print(s.name)
     print(s.shares)
+    # s.share = 234
 
--- structure.py
+++ structure.py
@@ -5,14 +5,6 @@ import inspect
 class Structure:
     _fields = ()
 
-    @staticmethod
-    def _init():
-        locs = sys._getframe(1).f_locals
-        self = locs.pop('self')
-        print(locs)
-        for name, val in locs.items():
-            setattr(self, name, val)
-
     def __repr__(self):
         return f"{type(self).__name__}{tuple(getattr(self, name) for name in self._fields)}"
 
@@ -23,10 +15,14 @@ class Structure:
             raise AttributeError(f'No attribute {name}')
 
     @classmethod
-    def set_fields(cls):
-        sig = inspect.signature(cls)
-        cls._fields = tuple(sig.parameters)
-
+    def create_init(cls):
+        argstr = ', '.join(cls._fields)
+        code = f'def __init__(self, {argstr}):\n'
+        for field in cls._fields:
+            code += f'    self.{field} = {field}\n'
+        locs = {}
+        exec(code, locs)
+        cls.__init__ = locs['__init__']
 
 
 if __name__ == '__main__':
@@ -34,3 +30,15 @@ if __name__ == '__main__':
     def add(x, y):
         """Adds two things"""
         return x + y
+
+
+    from collections import namedtuple
+    Stock = namedtuple('Stock', ['name', 'shares', 'price'])
+    s = Stock('GOOG', 100, 490.1)
+    print(s.name)
+
+    print(s.shares)
+
+    print(s[1])
+    import inspect
+    print(inspect.getsource(namedtuple))
