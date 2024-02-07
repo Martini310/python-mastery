cc229eb3f7badbdd870ab1cd80b1365bec878cc4
Martini310 <martin310@wp.pl>
2024-02-07 09:57:51 +0100

    Exercise 8_6

--- follow.py
+++ follow.py
@@ -3,15 +3,17 @@ import time
 
 
 def follow(filename):
-    f = open(filename)
-    f.seek(0, os.SEEK_END)   # Move file pointer 0 bytes from end of file
-
-    while True:
-        line = f.readline()
-        if line == '':
-            time.sleep(0.1)   # Sleep briefly and retry
-            continue
-        yield line
+    try:
+        with open(filename, 'r') as f:
+            f.seek(0, os.SEEK_END)
+            while True:
+                line = f.readline()
+                if line == '':
+                    time.sleep(0.1)  # Sleep briefly to avoid busy wait
+                    continue
+                yield line
+    except GeneratorExit:
+        print('Following Done')
 
 
 if __name__ == '__main__':
